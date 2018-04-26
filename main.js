13067dcac52042eabf77754f40b171fb94192600
Steven Pichardo <stevepichardo@outlook.com>
2018-04-26 18:41:22 -0400

    added local marker to map

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 var map;
+
+var myLatLng = { lat: 28.015210, lng: -81.968631};
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 28.039465, lng: -81.949804},
     zoom: 10
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
   });
 }
